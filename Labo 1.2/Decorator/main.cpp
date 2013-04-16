ea620e06315777a3862298338fc7a260392f0aa4
Alexandre Perez <alexandre.perez92@gmail.com>
2013-04-16 23:07:04 +0200

    add the most beautiful UI that you have ever seen

--- Labo 1.2/Decorator/main.cpp
+++ Labo 1.2/Decorator/main.cpp
@@ -1,34 +1,157 @@
 #include <iostream>
+#include <clocale>
 
+#include "include/panierdefruit.h"
+#include "include/fruit.h"
+#include "include/banane.h"
+#include "include/fraise.h"
 #include "include/pomme.h"
+
 #include "include/fruitdecoratoregal.h"
 #include "include/fruitdecoratorsharp.h"
 #include "include/fruitdecoratorstar.h"
 
 using namespace std;
 
+void ajouterFruits(PanierDeFruit *panier);
+int choixFruit();
+void afficherCredits();
+void decorerFruit(Fruit*&);
+void clear();
+
+void ajouterFruits(PanierDeFruit *panier)
+{
+    while(true)
+    {
+        afficherCredits();
+        int choix = choixFruit();
+
+        switch (choix)
+        {
+        case 1: //banane
+        {
+            Fruit *fruit = new Banane();
+            decorerFruit(fruit);
+            panier->ajouterFruit(fruit);
+            break;
+        }
+        case 2: //fraise
+        {
+            Fruit *fruit = new Fraise();
+            decorerFruit(fruit);
+            panier->ajouterFruit(fruit);
+            break;
+        }
+        case 3: //pomme
+        {
+            Fruit *fruit = new Pomme();
+            decorerFruit(fruit);
+            panier->ajouterFruit(fruit);
+            break;
+        }
+        case 4: //panier
+        {
+            PanierDeFruit *panierInterne = new PanierDeFruit();
+            ajouterFruits(panierInterne);
+            panier->ajouterFruit(panierInterne);
+            break;
+        }
+        case 5: //Fin panier
+            return;
+        case 6: //Quitter
+            break;
+        default:
+            cout << "Veuillez vérifier votre saisie" << endl;
+        }
+    }
+}
+
+void afficherCredits()
+{
+    clear();
+    cout << "|===========================================|" << endl
+         << "|Programme réalisé par :                    |" << endl
+         << "|                        - Diego Antognini  |" << endl
+         << "|                        - Alexandre Perez  |" << endl
+         << "|                        - Sébastien Vaucher|" << endl
+         << "|===========================================|" << endl;
+}
+
+void clear()
+{
+	cout << string(50, '\n' );
+}
+
+int choixFruit()
+{
+    int choix;
+    do
+    {
+        cout << "Menu principal : " << endl
+            << "1) Ajouté une banane" << endl
+            << "2) Ajouté une fraise" << endl
+            << "3) Ajouté une pomme" << endl
+            << "4) Ajouté un panier" << endl
+            << "5) Finir panier" << endl
+            << "6) Quitter" << endl << endl
+            << "Choix : ";
+        cin >> choix;
+    }while(choix < 1 || choix > 6);
+    return choix;
+}
+
+void decorerFruit(Fruit *&fruit)
+{
+    int choix;
+    while(choix != 4)
+    {
+        choix = 0;
+        do
+        {
+            cout << "Menu décoration : " << endl
+                << "1) Ajouté une décoration \"=\"" << endl
+                << "2) Ajouté une décoration \"#\"" << endl
+                << "3) Ajouté une décoration \"*\"" << endl
+                << "4) Quitté l'ajout de décoration" << endl
+                << "Choix : ";
+            cin >> choix;
+        }while(choix < 1 || choix > 4);
+
+        switch (choix)
+        {
+        case 1: //=
+            fruit = new FruitDecoratorEgal(fruit);
+            break;
+        case 2: //#
+            fruit = new FruitDecoratorSharp(fruit);
+            break;
+        case 3: //*
+            fruit = new FruitDecoratorStar(fruit);
+            break;
+        case 4: //stop décoration
+            return;
+        default:
+            cout << "Veuillez vérifier votre saisie" << endl;
+        }
+    }
+}
+
 int main()
 {
-    Pomme *p = new Pomme();
-    FruitDecoratorEgal *pde = new FruitDecoratorEgal(p);
-    FruitDecoratorSharp *pds = new FruitDecoratorSharp(pde);
-    FruitDecoratorStar *pdst = new FruitDecoratorStar(pds);
+    setlocale(LC_ALL, "frs");
+    afficherCredits();
 
-    p->afficherFruit();
-    cout << endl;
-    cout << endl;
-    pde->afficherFruit();
-    cout << endl;
-    cout << endl;
-    pds->afficherFruit();
-    cout << endl;
-    cout << endl;
-    pdst->afficherFruit();
+    PanierDeFruit* panierRacine = new PanierDeFruit();
 
-    delete p;
-    delete pde;
-    delete pds;
-    delete pdst;
+    ajouterFruits(panierRacine);
+
+    cout << endl << endl;
+    cout << "Contenu du panier racine :" << endl;
+    panierRacine->afficherFruit();
+    cout << endl;
+    cout << "Pépins : " << boolalpha << panierRacine->avecOuSansPepin();
+
+    delete panierRacine;
 
     return 0;
 }
