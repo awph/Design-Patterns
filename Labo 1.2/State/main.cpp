e77ea0c35c47659991f0a94902bb67ac75ccf4d2
Diego Antognini <diegoantognini@gmail.com>
2013-04-17 15:50:38 +0200

    Improve UI

--- Labo 1.2/State/main.cpp
+++ Labo 1.2/State/main.cpp
@@ -1,14 +1,38 @@
 #include "commande.h"
 
+#include <iostream>
 #include <clocale>
+#include <string>
+
+using namespace std;
+void clear()
+{
+	cout << string(50, '\n' );
+}
+
+void afficherCredits()
+{
+    clear();
+    cout << "|===========================================|" << endl
+         << "|Programme réalisé par :                    |" << endl
+         << "|                        - Diego Antognini  |" << endl
+         << "|                        - Alexandre Perez  |" << endl
+         << "|                        - Sébastien Vaucher|" << endl
+         << "|===========================================|" << endl << endl;
+}
 
 int main()
 {
     setlocale(LC_ALL, "frs");
 
+    afficherCredits();
     Commande c;
-    for(int i = 0;i < 5; ++i)//5 pour bien voir qu'il n'y a plus de changement d'état après l'état <<Envoyée>>
+    for(int i = 0;i < 4; ++i)//4 pour bien voir qu'il n'y a plus de changement d'état après l'état <<Envoyée>>
+    {
         c.traiterCommande();
+        cout << "Appuyer sur [Enter] pour passer à l'état suivant" << endl;
+        cin.get();
+    }
 
     return 0;
 }
