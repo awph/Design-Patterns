43acfb9cb3f3c0dcc805b3167853f22d929ab278
Sébastien Vaucher <seb.vaucher@gmail.com>
2013-04-17 14:46:55 +0200

    Composite menu

--- Labo 1.2/Composite/main.cpp
+++ Labo 1.2/Composite/main.cpp
@@ -5,33 +5,76 @@
 #include "include/fraise.h"
 #include "include/pomme.h"
 #include <clocale>
+#include <cstdlib>
 
 using namespace std;
+
+void clear()
+{
+	cout << string(50, '\n' );
+}
+
+void afficherCredits()
+{
+    clear();
+    cout << "|===========================================|" << endl
+         << "|Programme réalisé par :                    |" << endl
+         << "|                        - Diego Antognini  |" << endl
+         << "|                        - Alexandre Perez  |" << endl
+         << "|                        - Sébastien Vaucher|" << endl
+         << "|===========================================|" << endl;
+}
+
+int choixFruit()
+{
+    int choix;
+    do
+    {
+        cout << "Menu principal : " << endl
+            << "1) Ajouter une banane" << endl
+            << "2) Ajouter une fraise" << endl
+            << "3) Ajouter une pomme" << endl
+            << "4) Ajouter un panier" << endl
+            << "5) Finir panier" << endl
+            << "6) Quitter" << endl << endl
+            << "Choix : ";
+        cin >> choix;
+    }while(choix < 1 || choix > 6);
+    return choix;
+}
 
 void ajouterFruits(PanierDeFruit *panier)
 {
     while(true)
     {
-        cout << "Fruit à ajouter (banane, fraise, pomme, panier, finpanier) : " << endl;
-        string input;
-        cin >> input;
+        afficherCredits();
+        int choix = choixFruit();
 
-        if(input == "banane")
-            panier->ajouterFruit(new Banane());
-        else if(input == "fraise")
-            panier->ajouterFruit(new Fraise());
-        else if(input == "pomme")
-            panier->ajouterFruit(new Pomme());
-        else if(input == "panier")
+        switch (choix)
         {
-            PanierDeFruit *panierInterne = new PanierDeFruit();
-            ajouterFruits(panierInterne);
-            panier->ajouterFruit(panierInterne);
+            case 1: //banane
+                panier->ajouterFruit(new Banane());
+                break;
+            case 2: //fraise
+                panier->ajouterFruit(new Fraise());
+                break;
+            case 3: //pomme
+                panier->ajouterFruit(new Pomme());
+                break;
+            case 4: //panier
+            {
+                PanierDeFruit *panierInterne = new PanierDeFruit();
+                ajouterFruits(panierInterne);
+                panier->ajouterFruit(panierInterne);
+                break;
+            }
+            case 5: //Fin panier
+                return;
+            case 6: //Quitter
+                exit(EXIT_SUCCESS);
+            default:
+                cout << "Veuillez vérifier votre saisie" << endl;
         }
-        else if(input == "finpanier")
-            return;
-        else
-            cout << "Veuillez vérifier votre saisie" << endl;
     }
 }
 
